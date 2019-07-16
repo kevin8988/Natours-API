c64370f421c1bae9ac9d19ab9931d99667e88bf3
KevinChips <kevsilva07@gmail.com>
2019-07-16 11:05:22 -0300

    handling get request for tours

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   console.log('Request received');
   res.status(200).json({ message: 'Hey there!', app: 'Natours' });
 });
@@ -11,6 +12,21 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   res.status(200).send('You can post to this endpoint...');
 });
+ */
+
+const toursDataJSON = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: toursDataJSON.length,
+    data: {
+      tours: toursDataJSON
+    }
+  });
+});
+
+app.get('/api/v1/tours', (req, res) => {});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
