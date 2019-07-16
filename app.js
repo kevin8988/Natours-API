6e722c750e57c796b7971523a339edc9fec805ca
KevinChips <kevsilva07@gmail.com>
2019-07-16 12:28:17 -0300

    handling delete requests

--- app.js
+++ app.js
@@ -76,6 +76,21 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   res.status(200).send({ status: 'success', data: { tour: 'Updated tour...' } });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id >= tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  const tour = tours.find(el => el.id === id);
+
+  res.status(200).send({ status: 'success', data: { tour: 'Deleted tour...' } });
+});
+
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
