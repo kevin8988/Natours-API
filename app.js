8c05737820bb407ae534fd8919ec10bff7bd86a2
KevinChips <kevsilva07@gmail.com>
2019-07-16 11:42:58 -0300

    handling post requests to add a new tour

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const express = require('express');
 
-const app = express();
 const port = 3000;
+const app = express();
+app.use(express.json());
 
 /* app.get('/', (req, res) => {
   console.log('Request received');
@@ -14,19 +15,33 @@ app.post('/', (req, res) => {
 });
  */
 
-const toursDataJSON = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'));
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
-    results: toursDataJSON.length,
+    results: tours.length,
     data: {
-      tours: toursDataJSON
+      tours
     }
   });
 });
 
-app.get('/api/v1/tours', (req, res) => {});
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign(
+    {
+      id: newId
+    },
+    req.body
+  );
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    if (err) console.log(err);
+    res.status(201).json({ status: 'success', data: { tour: newTour } });
+  });
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
