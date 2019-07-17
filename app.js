15fb6587efdadc5fc3d4f5592b0f4158c822d14c
KevinChips <kevsilva07@gmail.com>
2019-07-17 12:27:08 -0300

    set environment variables

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const tourRouter = require(`${__dirname}/routes/tourRoutes`);
 const app = express();
 
 //1. Middlewares
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config({
+  path: `${__dirname}/config.env`
+});
+
 const app = require(`${__dirname}/app`);
 
-const port = 3000;
-
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
