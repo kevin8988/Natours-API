a087d08f137e091a9a7fdeed60bcf985982ff66c
KevinChips <kevsilva07@gmail.com>
2019-07-18 11:48:39 -0300

    find all tours

--- DAOs/tourDAO.js
+++ DAOs/tourDAO.js
@@ -4,3 +4,8 @@ exports.createTour = async data => {
   const newTour = await Tour.create(data);
   return newTour;
 };
+
+exports.getAllTours = async () => {
+  const allTours = await Tour.find();
+  return allTours;
+};
