c0b5d2bc3d233b20fdc588cf0a47671171ea4cd6
KevinChips <kevsilva07@gmail.com>
2019-07-18 10:34:21 -0300

    DB connection and first schema

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
+/* eslint-disable no-console */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
 const app = require('./app');
 
 dotenv.config({
   path: './config.env'
 });
 
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }).then(() => console.log('DB connection successful!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A tour must have a name'],
+    unique: true
+  },
+  price: {
+    type: Number,
+    require: [true, 'A tour must have a price']
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT;
-app.listen(port, () => {});
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
