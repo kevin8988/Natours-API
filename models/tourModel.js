ad742d3d0adfb499e657bdf9f930dff0f6a21928
KevinChips <kevsilva07@gmail.com>
2019-07-23 10:48:06 -0300

    document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -54,7 +55,8 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
       select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    slug: String
   },
   {
     toJSON: { virtuals: true },
@@ -65,6 +67,18 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function() {
   return Math.ceil(this.duration / 7);
 });
+
+//Document middleware: runs before the .save() and .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+/* //document middleware : runs after the .save() and .create()
+tourSchema.post('save', function(doc, next) {
+  console.log(doc);
+  next();
+}); */
 
 const Tour = mongoose.model('Tour', tourSchema);
 
