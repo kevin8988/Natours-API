42c4b4cead4233affb4fd2b5ede1d0980ebffbe4
KevinChips <kevsilva07@gmail.com>
2019-07-23 17:23:58 -0300

    catching error in async fn

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const tourDAO = require('./../DAOs/tourDAO');
+const catchAsync = require('./../utils/CatchAsync');
 
 exports.aliasTopTour = (req, res, next) => {
   req.query.limit = 5;
@@ -7,62 +8,38 @@ exports.aliasTopTour = (req, res, next) => {
   next();
 };
 
-exports.getAllTours = async (req, res) => {
-  try {
-    const tours = await tourDAO.getAllTours(req.query);
-    res.status(200).json({ status: 'success', results: tours.length, data: { tours } });
-  } catch (error) {
-    res.status(404).json({ status: 'fail', message: error });
-  }
-};
-exports.createTour = async (req, res) => {
-  try {
-    const newTour = await tourDAO.createTour(req.body);
-    res.status(201).json({ status: 'success', data: { tour: newTour } });
-  } catch (error) {
-    res.status(400).json({ status: 'fail', message: error });
-  }
-};
-exports.getTour = async (req, res) => {
-  try {
-    const tour = await tourDAO.getTour(req.params.id);
-    res.status(200).json({ status: 'success', data: { tour } });
-  } catch (error) {
-    res.status(400).json({ status: 'fail', message: error });
-  }
-};
-exports.deleteTour = async (req, res) => {
-  try {
-    await tourDAO.deleteTour(req.params.id);
-    res.status(204).json({ status: 'success', data: null });
-  } catch (error) {
-    res.status(400).json({ status: 'fail', message: error });
-  }
-};
-exports.updateTour = async (req, res) => {
-  try {
-    const tour = await tourDAO.updateTour(req.params.id, req.body);
-    res.status(200).json({ status: 'success', data: { tour } });
-  } catch (error) {
-    res.status(400).json({ status: 'fail', message: error });
-  }
-};
+exports.getAllTours = catchAsync(async (req, res, next) => {
+  const tours = await tourDAO.getAllTours(req.query);
+  res.status(200).json({ status: 'success', results: tours.length, data: { tours } });
+});
 
-exports.getTourStats = async (req, res) => {
-  try {
-    const status = await tourDAO.tourStats();
-    res.status(200).json({ status: 'success', data: { status } });
-  } catch (error) {
-    res.status(400).json({ status: 'fail', message: error });
-  }
-};
+exports.createTour = catchAsync(async (req, res) => {
+  const newTour = await tourDAO.createTour(req.body);
+  res.status(201).json({ status: 'success', data: { tour: newTour } });
+});
 
-exports.getMonthlyPlan = async (req, res) => {
-  try {
-    const year = req.params.year * 1;
-    const plan = await tourDAO.getMonthlyPlan(year);
-    res.status(200).json({ status: 'success', data: { plan } });
-  } catch (error) {
-    res.status(400).json({ status: 'fail', message: error });
-  }
-};
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await tourDAO.getTour(req.params.id);
+  res.status(200).json({ status: 'success', data: { tour } });
+});
+
+exports.deleteTour = catchAsync(async (req, res) => {
+  await tourDAO.deleteTour(req.params.id);
+  res.status(204).json({ status: 'success', data: null });
+});
+
+exports.updateTour = catchAsync(async (req, res) => {
+  const tour = await tourDAO.updateTour(req.params.id, req.body);
+  res.status(200).json({ status: 'success', data: { tour } });
+});
+
+exports.getTourStats = catchAsync(async (req, res) => {
+  const status = await tourDAO.tourStats();
+  res.status(200).json({ status: 'success', data: { status } });
+});
+
+exports.getMonthlyPlan = catchAsync(async (req, res) => {
+  const year = req.params.year * 1;
+  const plan = await tourDAO.getMonthlyPlan(year);
+  res.status(200).json({ status: 'success', data: { plan } });
+});
