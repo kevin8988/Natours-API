7b65744e57c650cad1fa7092f9fb659c07149c02
KevinChips <kevsilva07@gmail.com>
2019-07-23 15:51:19 -0300

    handling unhandled routes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = require('./app');
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 mongoose.connect(DB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }).then(() => console.log('DB connection successful!'));
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
 });
