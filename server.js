be5f00351aa36758c87989aeb8bd1aca57ec2adf
KevinChips <kevsilva07@gmail.com>
2019-07-24 16:44:11 -0300

    unhandled rejections and uncaught exceptions

--- server.js
+++ server.js
@@ -4,12 +4,28 @@ const dotenv = require('dotenv');
 dotenv.config({
   path: './config.env'
 });
+
+process.on('uncaughtException', err => {
+  console.log('Uncaught Exception... Shutting down...');
+  console.error(err);
+
+  process.exit(1);
+});
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 mongoose.connect(DB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }).then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server running on ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('Unhandled Rejection... Shutting down...');
+  console.error(err);
+  server.close(() => {
+    process.exit(1);
+  });
 });
