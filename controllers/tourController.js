b60872c69906a05ba6a961300cbcad0611fcb294
KevinChips <kevsilva07@gmail.com>
2019-07-24 14:47:42 -0300

    not found errors

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 const tourDAO = require('./../DAOs/tourDAO');
 const catchAsync = require('./../utils/CatchAsync');
+const AppError = require('./../utils/AppError');
 
 exports.aliasTopTour = (req, res, next) => {
   req.query.limit = 5;
@@ -13,32 +14,47 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
   res.status(200).json({ status: 'success', results: tours.length, data: { tours } });
 });
 
-exports.createTour = catchAsync(async (req, res) => {
+exports.createTour = catchAsync(async (req, res, next) => {
   const newTour = await tourDAO.createTour(req.body);
   res.status(201).json({ status: 'success', data: { tour: newTour } });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await tourDAO.getTour(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that id', 404));
+  }
+
   res.status(200).json({ status: 'success', data: { tour } });
 });
 
-exports.deleteTour = catchAsync(async (req, res) => {
-  await tourDAO.deleteTour(req.params.id);
+exports.deleteTour = catchAsync(async (req, res, next) => {
+  const tour = await tourDAO.deleteTour(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that id', 404));
+  }
+
   res.status(204).json({ status: 'success', data: null });
 });
 
-exports.updateTour = catchAsync(async (req, res) => {
+exports.updateTour = catchAsync(async (req, res, next) => {
   const tour = await tourDAO.updateTour(req.params.id, req.body);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that id', 404));
+  }
+
   res.status(200).json({ status: 'success', data: { tour } });
 });
 
-exports.getTourStats = catchAsync(async (req, res) => {
+exports.getTourStats = catchAsync(async (req, res, next) => {
   const status = await tourDAO.tourStats();
   res.status(200).json({ status: 'success', data: { status } });
 });
 
-exports.getMonthlyPlan = catchAsync(async (req, res) => {
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
   const year = req.params.year * 1;
   const plan = await tourDAO.getMonthlyPlan(year);
   res.status(200).json({ status: 'success', data: { plan } });
