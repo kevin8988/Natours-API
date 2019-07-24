d3a4bcc870666157fb43b661f63c020d145dc196
KevinChips <kevsilva07@gmail.com>
2019-07-24 16:10:29 -0300

    handle duplicate fields error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -17,6 +17,15 @@ const handleCastErrorDB = err => {
   return new AppError(`${err.value} is an invalid ${err.path}`, 400);
 };
 
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/);
+  return new AppError(`Duplicate field value ${value[0]}`, 400);
+};
+
+const handleValidatorErrorDB = err => {
+  return new AppError(err.message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -26,6 +35,8 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV.trim() === 'production') {
     let error = { ...err };
     if (error.name === 'CastError') error = handleCastErrorDB(err);
+    else if (error.code === 11000) error = handleDuplicateFieldsDB(err);
+    else if (error.name === 'ValidationError') error = handleValidatorErrorDB(err);
     sendErrorProd(error, res);
   }
 };
