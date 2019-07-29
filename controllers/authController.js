1e9a63620ce6a97dc5ea739cf4639f5e6544099b
KevinChips <kevsilva07@gmail.com>
2019-07-29 12:50:01 -0300

    json web token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,18 @@
+const jwt = require('jsonwebtoken');
 const userDAO = require('./../DAOs/userDAO');
 const catchAsync = require('./../utils/CatchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await userDAO.createUser(req.body);
-  res.status(201).json({ status: 'success', data: { user: newUser } });
+  const newUser = await userDAO.createUser({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+
+  res.status(201).json({ status: 'success', token, data: { user: newUser } });
 });
