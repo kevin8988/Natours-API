b3834488e711dff29adbc604d8fcf3b732d70895
KevinChips <kevsilva07@gmail.com>
2019-08-01 13:17:21 -0300

    requests rate limiting and http security

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
@@ -11,14 +13,30 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 //1. Middlewares
+
+//Security http header
+app.use(helmet());
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+//Limit requests for same api
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, Please try again in an hour!'
+});
+
+app.use('/api', limiter);
+
+//Body parser
+app.use(express.json({ limit: '10kb' }));
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 //2. Routes
+app.use(limiter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
