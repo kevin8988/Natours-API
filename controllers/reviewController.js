2cd828fbb446281f043e27639fdfbb5526e40cfc
KevinChips <kevsilva07@gmail.com>
2019-08-02 11:40:13 -0300

    create and all reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,12 @@
 const catchAsync = require('./../utils/CatchAsync');
+const reviewDAO = require('./../DAOs/reviewDAO');
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {});
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await reviewDAO.getAllReviews();
+  res.status(200).json({ status: 'success', results: reviews.length, data: { reviews } });
+});
 
-exports.createReview = catchAsync(async (req, res, next) => {});
+exports.createReview = catchAsync(async (req, res, next) => {
+  const review = await reviewDAO.createReview(req.body);
+  res.status(201).json({ status: 'success', data: { review } });
+});
