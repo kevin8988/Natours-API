be06dd034d2af7fc46197b212ad2b1e419a89a08
KevinChips <kevsilva07@gmail.com>
2019-08-02 11:34:00 -0300

    review controller

---
+++ controllers/reviewController.js
@@ -0,0 +1,5 @@
+const catchAsync = require('./../utils/CatchAsync');
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {});
+
+exports.createReview = catchAsync(async (req, res, next) => {});
