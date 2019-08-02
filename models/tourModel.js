edb6c3042e2399db07362146ae5b711974d36624
KevinChips <kevsilva07@gmail.com>
2019-08-02 11:11:56 -0300

    populate tour guides

--- models/tourModel.js
+++ models/tourModel.js
@@ -142,6 +142,11 @@ tourSchema.pre(/^find/, function(next) {
   next();
 });
 
+tourSchema.pre(/^find/, function(next) {
+  this.populate({ path: 'guides', select: '-__v -passwordChangedAt' });
+  next();
+});
+
 //Query middleware post
 tourSchema.post(/^find/, function(docs, next) {
   //console.log(`Query took ${Date.now() - this.start} milliseconds`);
