e5e512f7381c06159e903cf825dc71ad413570d2
KevinChips <kevsilva07@gmail.com>
2019-08-05 17:02:29 -0300

    adding missing authentication and authorization

--- controllers/authController.js
+++ controllers/authController.js
@@ -92,12 +92,12 @@ exports.protect = catchAsync(async (req, res, next) => {
 });
 
 exports.restrictTo = (...roles) => {
-  return catchAsync(async (req, res, next) => {
+  return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       return next(new AppError('You do not have permission to perform this action', 403));
     }
     next();
-  });
+  };
 };
 
 exports.forgotPassword = catchAsync(async (req, res, next) => {
