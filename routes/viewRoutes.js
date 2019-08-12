8072cba4039b05821f0be1a8915bc33e93c74fd4
KevinChips <kevsilva07@gmail.com>
2019-08-12 16:01:37 -0300

    building the overview template with tour data

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.route('/').get(viewController.getOverview);
 
-router.route('/tour').get(viewController.getTour);
+router.route('/tours/:tourSlug').get(viewController.getTour);
 
 module.exports = router;
