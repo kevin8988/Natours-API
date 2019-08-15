7372fffd282cacf71f171595223a94712b523090
KevinChips <kevsilva07@gmail.com>
2019-08-15 14:00:27 -0300

    logging user

--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,21 @@
 /* eslint-disable */
 
 const login = async (email, password) => {
-  console.log(email, password);
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      url: 'http://localhost:3000/api/v1/users/login',
       data: {
         email,
         password
       }
     });
-    console.log(res);
-  } catch (error) {
-    console.log(error.response.data);
-  }
+    if (res.data.status === 'success') {
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (error) {}
 };
 
 document.querySelector('.form').addEventListener('submit', event => {
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,9 +4,11 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
+router.use(authController.isLoggedIn);
+
 router.route('/').get(viewController.getOverview);
 
-router.route('/tours/:tourSlug').get(authController.protect, viewController.getTour);
+router.route('/tours/:tourSlug').get(viewController.getTour);
 
 router.route('/login').get(viewController.getLoginForm);
 
