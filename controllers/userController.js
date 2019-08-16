7230321a2e0481dd3d660cc1f21772291b967021
KevinChips <kevsilva07@gmail.com>
2019-08-16 17:04:56 -0300

    editing images with sharp

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,11 @@
 const multer = require('multer');
+const sharp = require('sharp');
 const catchAsync = require('./../utils/CatchAsync');
 const AppError = require('./../utils/AppError');
 const factory = require('./handlerFactory');
 const User = require('./../models/userModel');
 
-const multerStorage = multer.diskStorage({
+/* const multerStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'public/img/users');
   },
@@ -12,7 +13,9 @@ const multerStorage = multer.diskStorage({
     const ext = file.mimetype.split('/')[1];
     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
   }
-});
+}); */
+
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
@@ -29,6 +32,20 @@ const upload = multer({
 });
 
 exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+};
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
